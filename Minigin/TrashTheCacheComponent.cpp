7294df49635aaffb9ab24b861a0df10b4ba4c9b8
Artuur Demeyer <Artuur.Demeyer@Outlook.com>
2024-03-16 10:35:55 +0100

    finished Thrash The Cache assignment

--- Minigin/TrashTheCacheComponent.cpp
+++ Minigin/TrashTheCacheComponent.cpp
@@ -3,14 +3,45 @@
 #include "ImPlot.h"
 
 TrashTheCacheComponent::TrashTheCacheComponent(std::weak_ptr<GameObject> owner) :
-	Component{ owner }
+	Component{ owner },
+	m_BufferSize{ 1000000 },
+	m_StepSizes{ new float[11] { 1.0f, 2.0f, 4.0f, 8.0f, 16.0f, 32.0f, 64.0f, 128.0f, 256.0f, 512.0f, 1024.0f } },
+	m_IntegerData{ new float[11] { 1.0f, 2.0f, 4.0f, 8.0f, 16.0f, 32.0f, 64.0f, 128.0f, 256.0f, 512.0f, 1024.0f } },
+	m_GameObjectData{ new float[11] { 1.0f, 2.0f, 4.0f, 8.0f, 16.0f, 32.0f, 64.0f, 128.0f, 256.0f, 512.0f, 1024.0f } },
+	m_GameObjectAltData{ new float[11] { 1.0f, 2.0f, 4.0f, 8.0f, 16.0f, 32.0f, 64.0f, 128.0f, 256.0f, 512.0f, 1024.0f } },
+	m_ButtonsPressed{ new bool[2] { false, false } }
 {
+	int sum{};
+	Measure<int>([&sum](int x) -> void { sum += x; }, m_IntegerData);
+	Measure<GameObject3D>([&sum](GameObject3D& x) -> void { sum += x.id; }, m_GameObjectData);
+	Measure<GameObject3DAlt>([&sum](GameObject3DAlt& x) -> void { sum += x.id; }, m_GameObjectAltData);
+}
 
+TrashTheCacheComponent::~TrashTheCacheComponent()
+{
+	delete m_ButtonsPressed;
+	delete m_StepSizes;
+	delete m_IntegerData;
+	delete m_GameObjectData;
+	delete m_GameObjectAltData;
 }
 
 void TrashTheCacheComponent::Update(std::chrono::milliseconds deltaTime)
 {
 	deltaTime;
+
+	int sum{};
+	if (m_ButtonsPressed[0]) 
+	{
+		Measure<int>([&sum](int x) -> void { sum += x; }, m_IntegerData);
+		m_ButtonsPressed[0] = false;
+	}
+	if (m_ButtonsPressed[1])
+	{
+		Measure<GameObject3D>([&sum](GameObject3D& x) -> void { sum += x.id; }, m_GameObjectData);
+		Measure<GameObject3DAlt>([&sum](GameObject3DAlt& x) -> void { sum += x.id; }, m_GameObjectAltData);
+		m_ButtonsPressed[1] = false;
+	}
 }
 
 void TrashTheCacheComponent::FixedUpdate(std::chrono::milliseconds deltaTime)
@@ -20,5 +51,49 @@ void TrashTheCacheComponent::FixedUpdate(std::chrono::milliseconds deltaTime)
 
 void TrashTheCacheComponent::Render() const
 {
-	ImPlot::ShowDemoWindow();
+	ImGui::Begin("Exercise 1");
+
+	if (ImGui::Button("Resample"))
+	{
+		m_ButtonsPressed[0] = true;
+	}
+
+	if (ImPlot::BeginPlot("Integer buffer"))
+	{
+		ImPlot::SetNextMarkerStyle(ImPlotMarker_Circle);
+		ImPlot::PlotLine("Integer", m_StepSizes, m_IntegerData, 11);
+		ImPlot::EndPlot();
+	}
+	
+	ImGui::End();
+
+	ImGui::Begin("Exercise 2");
+
+	if (ImGui::Button("Resample"))
+	{
+		m_ButtonsPressed[1] = true;
+		m_ButtonsPressed[2] = true;
+	}
+
+	if (ImPlot::BeginPlot("GameObject buffer"))
+	{
+		ImPlot::SetNextMarkerStyle(ImPlotMarker_Circle);
+		ImPlot::PlotLine("GameObject", m_StepSizes, m_GameObjectData, 11);
+		ImPlot::EndPlot();
+	}
+	if (ImPlot::BeginPlot("GameObjectAlt buffer"))
+	{
+		ImPlot::SetNextMarkerStyle(ImPlotMarker_Circle);
+		ImPlot::PlotLine("GameObjectAlt", m_StepSizes, m_GameObjectAltData, 11);
+		ImPlot::EndPlot();
+	}
+	if (ImPlot::BeginPlot("GameObject Versus GameObjectAlt"))
+	{
+		ImPlot::SetNextMarkerStyle(ImPlotMarker_Circle);
+		ImPlot::PlotLine("GameObject", m_StepSizes, m_GameObjectData, 11);
+		ImPlot::PlotLine("GameObjectAlt", m_StepSizes, m_GameObjectAltData, 11);
+		ImPlot::EndPlot();
+	}
+
+	ImGui::End();
 }
--- Minigin/TrashTheCacheComponent.h
+++ Minigin/TrashTheCacheComponent.h
@@ -1,4 +1,11 @@
 #pragma once
+
+#include <chrono>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
 #include "Component.h"
 
 class TrashTheCacheComponent : public Component
@@ -6,7 +13,7 @@ class TrashTheCacheComponent : public Component
 public:
 
 	TrashTheCacheComponent(std::weak_ptr<GameObject> owner);
-	~TrashTheCacheComponent() = default;
+	~TrashTheCacheComponent();
 
 	TrashTheCacheComponent(const TrashTheCacheComponent&) = delete;
 	TrashTheCacheComponent(TrashTheCacheComponent&&) = delete;
@@ -19,6 +26,103 @@ public:
 
 private:
 
+	struct transform
+	{
+		float matrix[16]
+		{
+			1.0f, 0.0f, 0.0f, 0.0f,
+			0.0f, 1.0f, 0.0f, 0.0f,
+			0.0f, 0.0f, 1.0f, 0.0f,
+			0.0f, 0.0f, 0.0f, 1.0f
+		};
+	};
 
+	class GameObject3D
+	{
+	public:
+		transform local{};
+		int id{};
+	};
+
+	class GameObject3DAlt
+	{
+	public:
+		transform* local{};
+		int id{};
+	};
+
+	template<typename ObjectType, typename OperationType>
+	void Measure(OperationType operation, float* const data)
+	{
+		float measurements[11][10]{};
+
+		for (int indexMeasurement{}; indexMeasurement < 10; ++indexMeasurement)
+		{
+			ObjectType* buffer{ new ObjectType[m_BufferSize]{} };
+
+			int indexStepSize{};
+			for (int stepSize = 1; stepSize <= 1024; stepSize *= 2)
+			{
+				auto start{ std::chrono::steady_clock::now() };
+
+				for (size_t i = 0; i < m_BufferSize; i += stepSize)
+				{
+					operation(buffer[i]);
+				}
+
+				auto end{ std::chrono::steady_clock::now() };
+				auto time{ std::chrono::duration_cast<std::chrono::microseconds>(end - start) };
+
+				measurements[indexStepSize][indexMeasurement] = float(time.count());
+
+				++indexStepSize;
+			}
+
+			delete[] buffer;
+		}
+
+		for (int i{}; i < 11; ++i)
+		{
+			auto [minIter, maxIter] = std::minmax_element(&measurements[i][0], &measurements[i][10]);
+			float sum{ std::accumulate(&measurements[i][0], &measurements[i][10], 0.0f) };
+			float average{ (sum - *minIter - *maxIter) / 8.0f };
+			//std::cout << average << std::endl;
+			data[i] = average;
+		}
+	}
+
+	template<typename ObjectType, typename OperationType>
+	void MeasureOnce(OperationType operation, float* const data)
+	{
+		ObjectType* buffer{ new ObjectType[m_BufferSize]{} };
+
+		int indexStepSize{};
+		for (int stepSize = 1; stepSize <= 1024; stepSize *= 2)
+		{
+			auto start{ std::chrono::steady_clock::now() };
+
+			for (int i = 0; i < m_BufferSize; i += stepSize)
+			{
+				operation(buffer[i]);
+			}
+
+			auto end{ std::chrono::steady_clock::now() };
+			auto time{ std::chrono::duration_cast<std::chrono::microseconds>(end - start) };
+
+			data[indexStepSize] = float(time.count());
+			//std::cout << data[indexStepSize] << std::endl;
+
+			++indexStepSize;
+		}
+
+		delete[] buffer;
+	}
+
+	const size_t m_BufferSize;
+	float* m_StepSizes;
+	float* m_IntegerData;
+	float* m_GameObjectData;
+	float* m_GameObjectAltData;
+	bool* m_ButtonsPressed;
 };
 
